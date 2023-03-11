f98755f2293807209ea274007aece1ba0e3ecac3
armitage6 <pablutus@gmail.com>
2023-03-11 17:07:31 +0000

    subiendo todo list python flask

---
+++ src/app.py
@@ -0,0 +1,50 @@
+from flask import Flask, jsonify, request, json
+app = Flask(__name__)
+
+todos = [{ "label": "My first task", "done": False }]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    return jsonify(todos)
+
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = json.loads(request.data)
+    todos.append(request_body)
+    return jsonify(todos)
+    
+
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ",position)
+    todos.pop((position-1))
+    return jsonify(todos)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
